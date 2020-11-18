d829ab035135b15dee80568c3f298ec148bf755c
Kirill <kirillpnh0911@gmail.com>
2020-11-18 18:56:23 +0300

    completed

---
+++ Lab1/Lab1/Calc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    class Calc
+    {
+        static public void Calculator()
+        {
+            double firstnumber, secondnumber;
+            Console.WriteLine("Введите первое число");
+            firstnumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            secondnumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите действие: '+', '-', '*', '/'");
+            string act = Console.ReadLine();
+
+            switch (act)
+            {
+                case "+":
+                    Console.WriteLine("Результат вычислений: {0}", firstnumber + secondnumber);
+                    break;
+                case "-":
+                    Console.WriteLine("Результат вычислений: {0}", firstnumber - secondnumber);
+                    break;
+                case "*":
+                    Console.WriteLine("Результат вычислений: {0}", firstnumber * secondnumber);
+                    break;
+                case "/":
+                    if (secondnumber == 0)
+                        Console.WriteLine("На ноль делить нельзя");
+                    else
+                        Console.WriteLine("Результат вычислений: {0}", firstnumber / secondnumber);
+                    break;
+                default:
+                    Console.WriteLine("Данное действие не поддерживается калькулятором");
+                break;
+            }
+        }
+    }
+}
