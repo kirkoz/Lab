e44b91a328c01f62dd700257efdcf43fe8dcc6a9
Kirill <kirillpnh0911@gmail.com>
2020-12-03 02:21:40 +0300

    completed NUnit

---
+++ NUnit/Calc/Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calc
+{
+    public static class Calc
+    {
+        static public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        static public int Diffrence(int a, int b)
+        {
+            return a - b;
+        }
+
+        static public int Мultiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        static public int Division(int a, int b)
+        {
+            if (b == 0)
+                throw new System.DivideByZeroException();
+            return a / b;
+        }
+    }
+}
