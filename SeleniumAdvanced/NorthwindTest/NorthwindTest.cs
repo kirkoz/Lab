f4867280ea5f7af3ec4842f9300e640e8e05b58e
Kirill <kirillpnh0911@gmail.com>
2020-12-15 02:38:35 +0300

    completed selenium advanced

---
+++ SeleniumAdvanced/NorthwindTest/NorthwindTest.cs
@@ -0,0 +1,84 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverAdvanced.Page_Object;
+
+namespace NorthwindTest
+{
+
+    [TestFixture]
+    public class NorthwindTests
+    {
+        private IWebDriver driver;
+        private LoginPage loginpage;
+        private HomePage homepage;
+        private AllProductsPage allproductspage;
+        private NewProductPage newproductpage;
+        private EditProductPage editproductpage;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5000");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test, Order(1)]
+        public void TestLogin()
+        {
+            loginpage = new LoginPage(driver);
+            homepage = new HomePage(driver);
+            loginpage.Login("user", "user");
+            Assert.AreNotEqual("Login", homepage.H2HomePageText());
+        }
+
+        [Test, Order(2)]
+        public void TestCreateProduct()
+        {
+            allproductspage = new AllProductsPage(driver);
+            newproductpage = new NewProductPage(driver);
+            homepage.GoToAllProducts();
+            allproductspage.GoToNewProduct();
+            newproductpage.CreateProduct("Salmon", "Seafood", "Mayumi's", "100", "2", "3", "4", "5");
+            Assert.AreNotEqual("Product editing", allproductspage.H2AllProductsText());
+        }
+
+        [Test, Order(3)]
+        public void CheckFieldsProduct()
+        {
+            editproductpage = new EditProductPage(driver);
+            allproductspage.GoToSalmon();
+            Assert.AreEqual("Salmon", editproductpage.ProductName());
+            Assert.AreEqual("Seafood", editproductpage.CategoryID());
+            Assert.AreEqual("Mayumi's", editproductpage.SupplierID());
+            Assert.AreEqual("100,0000", editproductpage.UnitPrice());
+            Assert.AreEqual("2", editproductpage.QuantityPerUnit());
+            Assert.AreEqual("3", editproductpage.UnitsInStock());
+            Assert.AreEqual("4", editproductpage.UnitsOnOrder());
+            Assert.AreEqual("5", editproductpage.ReorderLevel());
+            Assert.AreEqual("true", editproductpage.Discontinued());
+        }
+        [Test, Order(4)]
+        public void RemoveProduct()
+        {
+            editproductpage.GoToProducts();
+            allproductspage.DeleteProduct();
+            Assert.Throws<OpenQA.Selenium.InvalidSelectorException>(() => driver.FindElement(By.XPath("=//td/a[text()=\"Salmon\"]")));
+        }
+        
+        [Test, Order(5)]
+        public void LogOut()
+        {
+            allproductspage.LogOut();
+            Assert.AreEqual("Login", loginpage.H2LoginPageText());
+        }
+        
+        [OneTimeTearDown]
+        public void CloseDriver()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumAdvanced/NorthwindTest/Page Object/AllProductsPage.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriverAdvanced.Page_Object
+{
+    class AllProductsPage
+    {
+        private IWebDriver driver;
+
+        public AllProductsPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private IWebElement H2FieldAllProducts => driver.FindElement(By.XPath("//h2"));
+
+        private IWebElement CreateNewButton => driver.FindElement(By.LinkText("Create new"));
+
+        private IWebElement SalmonObjectButton => driver.FindElement(By.LinkText("Salmon"));
+
+        private IWebElement SalmonObjectDeleteButton => driver.FindElement(By.XPath("//*[a[text()=\"Salmon\"]]/following-sibling::*[10]/a[text()=\"Remove\"]"));
+
+        private IWebElement LogOutButton => driver.FindElement(By.LinkText("Logout"));
+
+        public void GoToNewProduct()
+        {
+            new Actions(driver).Click(CreateNewButton).Build().Perform();
+        }
+
+        public string H2AllProductsText()
+        {
+            return H2FieldAllProducts.Text;
+        }
+
+        public void GoToSalmon()
+        {
+            new Actions(driver).Click(SalmonObjectButton).Build().Perform();
+        }
+
+        public void DeleteProduct()
+        {
+            new Actions(driver).Click(SalmonObjectDeleteButton).Build().Perform();
+            driver.SwitchTo().Alert().Accept();
+        }
+
+        public void LogOut()
+        { 
+            new Actions(driver).Click(LogOutButton).Build().Perform();
+        }
+    }
+}
