6d29dfe82f85aaddd7cc7d29225ad11b7b3e4c17
Kirill <kirillpnh0911@gmail.com>
2020-12-18 23:37:56 +0300

    completed webdriver framework

---
+++ WebDriverFramework/NorthwindTest/Page Object/EditProductPage.cs
@@ -0,0 +1,72 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriverAdvanced.Page_Object
+{
+    class EditProductPage : ProductPage
+    {
+
+        public EditProductPage(IWebDriver driver) : base(driver)
+        {
+        }
+       
+        private IWebElement CategoryIdField => driver.FindElement(By.XPath("//*[@id=\"CategoryId\"]/option[9]"));
+
+        private IWebElement SupplierIdField => driver.FindElement(By.XPath("//*[@id=\"SupplierId\"]/option[7]"));
+        
+        private IWebElement ProductsButton => driver.FindElement(By.XPath("//*[@href=\"/Product\"]"));
+
+        public string ProductName()
+        {
+            return ProductNameField.GetAttribute("value"); 
+        }
+
+        public string CategoryID()
+        {
+            return CategoryIdField.Text;
+        }
+        
+        public string SupplierID()
+        {
+            return SupplierIdField.Text;
+        }
+
+        public string UnitPrice()
+        {
+            return UnitPriceField.GetAttribute("value");
+        }
+
+        public string QuantityPerUnit()
+        {
+            return QuantityPerUnitField.GetAttribute("value"); 
+        }
+
+        public string UnitsInStock()
+        {
+            return UnitsInStockField.GetAttribute("value");
+        }
+
+        public string UnitsOnOrder()
+        {
+            return UnitsOnOrderField.GetAttribute("value");
+        }
+
+        public string ReorderLevel()
+        {
+            return ReorderLevelField.GetAttribute("value");
+        }
+
+        public string Discontinued()
+        {
+            return DiscontinuedField.GetAttribute("checked");
+        }
+
+        public void GoToProducts()
+        {
+            new Actions(driver).Click(ProductsButton).Build().Perform();
+        }
+    }
+}
---
+++ WebDriverFramework/NorthwindTest/Page Object/HomePage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriverAdvanced.Page_Object
+{
+    class HomePage
+    {
+        private IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        
+        private IWebElement H2FieldHomePage => driver.FindElement(By.XPath("//h2"));
+
+        private IWebElement AllProductsButton => driver.FindElement(By.LinkText("All Products"));
+
+        public string H2HomePageText()
+        {
+            return H2FieldHomePage.Text;
+        }
+
+        public void GoToAllProducts()
+        {
+            new Actions(driver).MoveToElement(AllProductsButton).Click(AllProductsButton).Build().Perform();
+        }
+
+    }
+}
